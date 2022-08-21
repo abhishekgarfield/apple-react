1d4345114dfa918bc98c2aabcd975b1b6e7fe95d
abhishekgarfield <abhishek2759@gmail.com>
2022-08-21 11:13:46 +0530

    components/navbar completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,77 @@
-const Navbar=()=>{
-    return(
-        <>
-        </>
-    );
-}
+const Navbar = () => {
+  return (
+    <>
+      <div class="nav-cont">
+        <ul>
+          <li>
+            <a class="logo" href="#">
+              <span class="hde">logo</span>
+            </a>
+          </li>
+          <li>
+            <a class="store" href="#">
+              <span class="hde">Store</span>
+            </a>
+          </li>
+          <li>
+            <a class="mac" href="#">
+              <span class="hde">Mac</span>
+            </a>
+          </li>
+          <li>
+            <a class="iPad" href="#">
+              <span class="hde">iPad</span>
+            </a>
+          </li>
+          <li>
+            <a class="iPhone" href="#">
+              <span class="hde">iPhone</span>
+            </a>
+          </li>
+          <li>
+            <a class="iwatch" href="#">
+              <span class="hde">Watch</span>
+            </a>
+          </li>
+          <li>
+            <a class="airpods" href="#">
+              <span class="hde">Airpods</span>
+            </a>
+          </li>
+          <li>
+            <a class="tv" href="#">
+              <span class="hde">Tv and Home</span>
+            </a>
+          </li>
+          <li>
+            <a class="only" href="#">
+              <span class="hde">Only on Apple</span>
+            </a>
+          </li>
+          <li>
+            <a class="accessories" href="#">
+              <span class="hde">Accessories</span>
+            </a>
+          </li>
+          <li>
+            <a class="support" href="#">
+              <span class="hde">Support</span>
+            </a>
+          </li>
+          <li>
+            <a class="search" href="#">
+              <span class="hde">Search</span>
+            </a>
+          </li>
+          <li>
+            <a class="bag" href="#">
+              <span class="hde">bag</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
