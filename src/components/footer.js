897b7a50ba391042c573206b5dd1fe9ff93b0b8b
abhishekgarfield <abhishek2759@gmail.com>
2022-08-21 09:21:54 +0530

    components/footer completed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,129 @@
 const Footer=()=>{
     return(
         <>
+            <footer>
+                <div class="foot_cont1">
+                    <div class="foot_cont2">
+                        <p>
+                            ‡Instant cashback of 5% of order value is available for qualifying HDFC Bank Credit Cards & HDFC Credit Card EMI only.
+                            Offer is limited to Maximum Instant Cashback Amount of ₹6000 per successful single order of ₹54900 and above.
+                            Instant cashback is available for up to two orders per 90-day period with an eligible card. 
+                            Any additional orders may be cancelled. Card eligibility is subject to terms and conditions between you and your card-issuing bank.
+                            Total transaction value is calculated after any trade-in credit or eligible discount is applied. 
+                            Any subsequent order adjustment(s) or cancellation(s) may result in instant cashback being recalculated,
+                            and any refund may be adjusted to account for instant cashback reclaim, which may result in no refund being made to you. 
+                            Offer may be revised or withdrawn at any time without any prior notice. Terms & conditions apply.
+                            Offer cannot be combined with Apple Store for Education or Corporate Employee Purchase Plan pricing.
+                            Multiple separate orders cannot be combined for instant cashback.
+                        </p>
+                        <ol>
+                            <li>
+                                Qualified purchasers can receive promotion savings when they purchase an eligible Mac or eligible iPad with eligible AirPods at a qualifying location.
+                                Only one pair of AirPods per eligible Mac or eligible iPad per qualified purchaser. Offer is subject to availability. 
+                                Subject to terms and conditions herein.
+                            </li>
+                            <li>
+                                ₹99/month after free trial. Only one offer per Apple ID and only one offer per family if you are part of a Family Sharing group, regardless of the number of devices you or your family purchases. 
+                                Offer is valid for 3 months after eligible device activation. Plan automatically renews until cancelled. Restrictions and other terms apply.
+                            </li>
+                        </ol>
+                        <p>
+                            ₹99/month after free trial. Only one offer per Apple ID and only one offer per family if you are part of a Family Sharing group, 
+                            regardless of the number of devices you or your family purchases. This offer is not available if you or your Family have previously accepted an Apple TV+ 1-year-free offer.
+                            Offer is valid for 3 months after eligible device activation. Plan automatically renews until cancelled. Restrictions and other terms apply.
+                        </p>
+                    </div>
+                    <hr></hr>
+                    <div class="foot_cont3">
+                        <div class="foot_list1">
+                            <ul>
+                                <li><b>Shop and learn</b></li>
+                                <li>Store</li>
+                                <li>Mac</li>
+                                <li>iPad</li>
+                                <li>iPhone</li>
+                                <li>Watch</li>
+                                <li>Airpods</li>
+                                <li>Tv and Home</li>
+                                <li>Airtags</li>
+                                <li>Accessories</li>
+                            </ul>
+                        </div>
+                        <div class="foot_list2">
+                            <ul>
+                                <li><b>Services</b></li>
+                                <li>Apple Music</li>
+                                <li>Apple TV+</li>
+                                <li>Apple Arcade</li>
+                                <li>iCloud</li>
+                                <li>Apple one</li>
+                                <li>Apple books</li>
+                                <li>Apple Podcasts</li>
+                                <li>App store</li>
+                            </ul>
+                            <ul>
+                                <li><b>Account</b></li>
+                                <li>Manage your apple id</li>
+                                <li>Apple store account</li>
+                                <li>iCloud.com</li>
+                            </ul>
+                        </div>
+                        <div class="foot_list3">
+                            <ul>
+                                <li><b>Apple store</b></li>
+                                <li>Ways to buy</li>
+                                <li>Apple trade in</li>
+                                <li>Recycling Programme</li>
+                                <li>Order status</li>
+                                <li>Shopping help</li>
+                            </ul>
+                        </div>
+                        <div class="foot_list4">
+                            <ul>
+                                <li><b>For buisness</b></li>
+                                <li>Apple and buisness</li>
+                            </ul>
+                            <ul>
+                                <li><b>For education</b></li>
+                                <li>Apple and education</li>
+                                <li>Shop for education</li>
+                                <li>Shop for university</li>
+                            </ul>
+                            <ul>
+                                <li><b>for Healthcare</b></li>
+                                <li>Apple in Healthcare</li>
+                                <li>Health in Apple watch</li>
+                            </ul>
+                        </div>
+                        <div class="foot_list5">
+                            <ul>
+                                <li><b>Apple values</b></li>
+                                <li>Accessability</li>
+                                <li>Enviornment</li>
+                                <li>Privacy</li>
+                                <li>Supplier responsibility</li>
+                            </ul>
+                            <ul>
+                                <li><b>About Apple</b></li>
+                                <li>Newsroom</li>
+                                <li>Apple leadership</li>
+                                <li>Career Oppertunities</li>
+                                <li>Investors</li>
+                                <li>Ethics and compliance</li>
+                                <li>Events</li>
+                                <li>Contact Apple</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="foot_cont4">
+                        <p>More ways to shop: Find a retailer near you. Or call  +91-9459163151 .</p>
+                    </div>
+                    <hr></hr>
+                    <div class="foot_cont5">
+                        <p>Copyright  &copy; 2022 abhishekgarfield. All rights reserved.</p>
+                    </div>
+                </div>
+            </footer>
         </>
     );
 }
