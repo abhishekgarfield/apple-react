b701bc2f2bccf1b3b5cb056ae5a992ed5625edc5
abhishekgarfield <abhishek2759@gmail.com>
2022-10-25 09:35:27 +0530

    reducer

--- src/Reducers/basketslice.js
+++ src/Reducers/basketslice.js
@@ -1,1 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    items:null
+}
+
+const basket=createSlice({
+    name:"basket",
+    initialState,
+    reducers:{
+        additems(state,action){
+            state.items=action.payload;
+        },
+        removeitems:(state,action)=>{
+            state.items=action.payload;
+        }
+    }
+})
+
+export const {additems,removeitems}=basket.actions;
+export default basket.reducer;
