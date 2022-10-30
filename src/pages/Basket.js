9c702123d8f31ac3927f9cb547e033d60f711fb9
abhishekgarfield <abhishek2759@gmail.com>
2022-10-30 16:07:45 +0530

    basket

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,13 +1,22 @@
 import { useMemo, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../components/navbar";
+import { additems, removeitems } from "../Reducers/basketslice";
 
 const Basket = () => {
+  const dispatch=useDispatch();
   const [Groupeditems, setGroupeditems] = useState(null);
   const items = useSelector((state) => {
     return state.basket.items;
   });
-  console.log(items);
+ const totalprice=useSelector((state)=>{
+  var totalprice=0
+  state.basket.items.map(({product_price})=>{
+        totalprice+=product_price;
+  })
+  return totalprice
+})
+
 
   const GroupItems = () => {
     const temp = {};
@@ -25,8 +34,41 @@ const Basket = () => {
   }, [items]);
   console.log(Groupeditems);
   return (
-    <div className="basket-container">
+    <div className="Basketcont">
       <Navbar />
+      <div className="basket-container">
+        <div className="Basket-items-container">
+          <div className="item-count">{`Quantity`}</div>
+          <div className="Item-name">Item</div>
+          <div className="Item-price">Price</div>
+        </div>
+        {Groupeditems &&
+          Object.entries(Groupeditems)?.map(([key, value], index) => {
+            return (
+              <div className="Basket-items-container">
+                <div className="item-count">
+                  <i className="fa fa-plus" onClick={()=>{
+                    dispatch(additems(value[0]))
+                  }}></i>
+                  <span>{`x${value.length}`}</span>
+                  <i className="fa fa-minus" onClick={()=>{
+                    dispatch(removeitems(value[0]))
+                  }}></i>
+                </div>
+                <div className="Item-name">{value[0]["Product name"]}</div>
+                <div className="Item-price">
+                  {`₹${value[0].product_price * value.length}` }
+                </div>
+              </div>
+            );
+          })}
+          <div className="Totalinfo">
+          <span> {`Total price ₹${totalprice}`}</span> 
+          <span>{`Delivery fee ₹${200}`}</span> 
+          <span>{`Discount ₹${2000}`}</span> 
+          <span> {`Total ₹${totalprice-200-2000}`}</span> 
+          </div>
+      </div>
     </div>
   );
 };
