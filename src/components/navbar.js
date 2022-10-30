7bb77ef09c4370c014f3e9de8534ec47e0cf8d78
abhishekgarfield <abhishek2759@gmail.com>
2022-10-30 13:34:14 +0530

    BASKET

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,124 +1,114 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const dispatch=useDispatch();
-  const systheme=useSelector((state)=>{return state.dark.theme})
-  const navigate=useNavigate();
-    const handleClick=(e)=>
-    {
-        var el=document.getElementsByClassName("nav-cont");
-        if(el[0].className==="nav-cont")
-        {
-            el[0].className+=" responsive";
-        }
-        else{
-            el[0].className="nav-cont"
-        }
+  const dispatch = useDispatch();
+  const systheme = useSelector((state) => {
+    return state.dark.theme;
+  });
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    var el = document.getElementsByClassName("nav-cont");
+    if (el[0].className === "nav-cont") {
+      el[0].className += " responsive";
+    } else {
+      el[0].className = "nav-cont";
     }
+  };
   return (
     <>
       <div className="nav-cont">
         <ul>
-        <li>
-            <span id="Phone-nav-bar"  onClick={handleClick}>
+          <li>
+            <span id="Phone-nav-bar" onClick={handleClick}>
               <span className="fa fa-bars"></span>
             </span>
           </li>
           <li>
-            <span className="logo" onClick={() => {
-              
-              navigate(`/`);
-              window.location.reload();
-              
-              
-            }} >
+            <span
+              className="logo"
+              onClick={() => {
+                navigate(`/`);
+              }}
+            >
               <span className="hde">logo</span>
             </span>
           </li>
           <li>
-            <span className="bag1" >
+            <span className="bag1">
               <span className="hde">bag</span>
             </span>
           </li>
           <li>
-            <span className="store" onClick={() => {
-              
-          navigate(`/listing/all`);
-          window.location.reload();
-          
-          
-        }} >
-              <span className="hde" >Store</span>
-            </span>
+            <Link to={`/listing/all`}>
+              <span className="store">
+                <span className="hde">Store</span>
+              </span>
+            </Link>
           </li>
           <li>
-            <span className="mac" onClick={() => {
-          navigate(`/listing/3`);
-          window.location.reload();
-        }}>
-              <span className="hde">Mac</span>
-            </span>
+            <Link to={`/listing/3`}>
+              <span className="mac">
+                <span className="hde">Mac</span>
+              </span>
+            </Link>
           </li>
           <li>
-            <span className="iPad" onClick={() => {
-          navigate(`/listing/2`);
-          window.location.reload();
-        }}>
-              <span className="hde">iPad</span>
-            </span>
+            <Link to={`/listing/2`}>
+              <span className="iPad">
+                <span className="hde">iPad</span>
+              </span>
+            </Link>
           </li>
           <li>
-            <span className="iPhone" onClick={() => {
-          navigate(`/listing/1`);
-          window.location.reload();
-        }}>
-              <span className="hde">iPhone</span>
-            </span>
+            <Link to={`/listing/1`}>
+              {" "}
+              <span className="iPhone">
+                <span className="hde">iPhone</span>
+              </span>
+            </Link>
           </li>
           <li>
-            <span className="iwatch" >
+            <span className="iwatch">
               <span className="hde">Watch</span>
             </span>
           </li>
           <li>
-            <span className="airpods" onClick={() => {
-          navigate(`/listing/5`);
-          window.location.reload();
-        }} >
-              <span className="hde">Airpods</span>
-            </span>
+            <Link to={`/listing/5`}>
+              <span className="airpods">
+                <span className="hde">Airpods</span>
+              </span>
+            </Link>
           </li>
           <li>
-            <span className="tv" >
+            <span className="tv">
               <span className="hde">TvanHome</span>
             </span>
           </li>
           <li>
-            <span className="only" >
+            <span className="only">
               <span className="hde">OnlyonApple</span>
             </span>
           </li>
           <li>
-            <span className="accessories" onClick={() => {
-          navigate(`/listing/5`);
-          window.location.reload();
-        }} >
-              <span className="hde">Accesories</span>
-            </span>
+            <Link to={`/listing/5`}>
+              <span className="accessories">
+                <span className="hde">Accesories</span>
+              </span>
+            </Link>
           </li>
           <li>
-            <span className="support" >
+            <span className="support">
               <span className="hde">Support</span>
             </span>
           </li>
           <li>
-            <span className="search" >
+            <span className="search">
               <span className="hde">Search</span>
             </span>
           </li>
           <li>
-            <span className="bag2" >
+            <span className="bag2">
               <span className="hde">bag</span>
             </span>
           </li>
--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,26 +1,29 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "../components/navbar";
 
 const Basket = () => {
+  const [Groupeditems, setGroupeditems] = useState(null);
   const items = useSelector((state) => {
     return state.basket.items;
   });
-
- const GroupItems=()=>{
-    const temp={};
-    items.forEach((product) => {
-        if(!temp[product.id])
-        {
-            temp[product.id]=[]
-        }
-        temp[product.id].push(product)
-    });
- }
- useMemo(()=>{
- GroupItems();
- },[items])
   console.log(items);
+
+  const GroupItems = () => {
+    const temp = {};
+    items.forEach((product) => {
+      if (!temp[product.Product_id]) {
+        temp[product.Product_id] = [];
+      }
+      temp[product.Product_id].push(product);
+    });
+    console.log(temp);
+    setGroupeditems(temp);
+  };
+  useMemo(() => {
+    GroupItems();
+  }, [items]);
+  console.log(Groupeditems);
   return (
     <div className="basket-container">
       <Navbar />
